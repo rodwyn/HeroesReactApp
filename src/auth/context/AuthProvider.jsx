0f91429eeecffa43b994916f24fbda4cc67575b9
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-10-04 17:10:15 -0500

    Added login and logout behavior.

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,16 +1,46 @@
 import { useReducer } from "react";
+import { types } from "../types/types";
 import { AuthContext } from "./AuthContext"
 import { AuthReducer } from "./AuthReducer";
 
-const initialState = {
-  logged: false
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return {
+    logged: !!user,
+    user
+  }
 };
 
 export const AuthProvider = ({children}) => {
-  const [authState, dispatch] = useReducer( AuthReducer, initialState);
+  const [authState, dispatch] = useReducer( AuthReducer, {}, init);
+
+  const login = (name='') => {
+    const user = { id: 123, name };
+    const action = {
+      type: types.login,
+      payload: user
+    };
+
+    localStorage.setItem('user', JSON.stringify(user));
+
+    dispatch(action)
+  };
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    const action = {
+      type: types.logout
+    };
+    dispatch(action);
+  };
   
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{
+      ...authState,
+      login,
+      logout
+    }}>
       {children}
     </AuthContext.Provider>
   )
--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
+  const {login} = useContext( AuthContext);
   const navigate = useNavigate();
 
   const onLoging = ()=> {
+    login('Rodwyn M')
     navigate('/', {
       replace: true
     })
