096c04449fd610a270e3fe42eaa8677e1b550b79
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-10-04 09:36:44 -0500

    Added context and reducer.

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,9 +1,10 @@
+import { AuthProvider } from "./auth"
 import { AppRouter } from "./heroes/router/AppRouter"
 
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
       <AppRouter />
-    </>
+    </AuthProvider>
   )
 }
